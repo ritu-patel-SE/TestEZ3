06a0447e6f57d3825bca73d34f83ebf868e7f4b1
ritu <ritu.patel@spec-india.com>
2023-09-05 18:08:32 +0530

    changes

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -2,5 +2,5 @@ import React from 'react'
 import styles from './button.module.css'
 
 export const ButtonComponent = ({ text, description }) => {
-  return <div className={styles.card}> Product number: {text} Product number: {description} </div>
+  return  <div className={styles.card}> <h4>Product number: {text}</h4> <p>Product number: {description} </p></div>
 }
